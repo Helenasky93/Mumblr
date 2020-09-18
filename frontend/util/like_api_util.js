4460e9984fe8b5137988334f7a7d9100965f49da
Helena Zarazua <helenasky1993@gmail.com>
2020-09-18 11:13:40 -0700

    added util and actions for likes

---
+++ frontend/util/like_api_util.js
@@ -0,0 +1,30 @@
+export const fetchAllLikes = () => {
+    $.ajax({
+        url:'api/likes',
+        method:'GET'
+    })
+}
+
+export const receiveLike = (id) => {
+    $.ajax({
+        url:`api/likes/${id}`,
+        method:'GET'
+    })
+}
+
+export const likePost = (id) => {
+    $.ajax({
+        url:'api/likes',
+        method:'POST',
+        data: {id}
+    })
+}
+
+export const unlikePost = (id) => {
+    $.ajax({
+        url:`api/likes${id}`,
+        method:'DELETE',
+        data:{id}
+    })
+}
+
